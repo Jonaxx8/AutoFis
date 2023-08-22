d74c5a7b6ed1ef1644d5ff6837dd1adba2e6585f
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2023-08-22 14:58:39 +0530

    changed splash icon to lottie icon

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:auto_fis/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
-
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,18 +11,19 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return AnimatedSplashScreen(
-      splash: Column(
-        children: [
-          Image.asset(
-            'assets/fisherma.jpg',
-            width: 300,
-            height: 300,
-          ),
-        ],
-      ),
+      splash: Lottie.asset('assets/splash_animation.json'),
+      // Column(
+      //   children: [
+      //     Image.asset(
+      //       'assets/fisherma.jpg',
+      //       width: 300,
+      //       height: 300,
+      //     ),
+      //   ],
+      // ),
       nextScreen: const MainScreen(),
       splashIconSize: 300,
-      duration: 3000,
+      duration: 2000,
       animationDuration: const Duration(seconds: 2),
       splashTransition: SplashTransition.fadeTransition,
       pageTransitionType: PageTransitionType.fade,
